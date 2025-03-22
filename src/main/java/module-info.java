62e010ef02006e010ae8f9ffd1ea79b1a20050b8
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-03-22 20:54:18 +0530

    Started the TheMentalTheraphyHelthFinal project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.thementaltheraphyhelthfinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.thementaltheraphyhelthfinal to javafx.fxml;
+    exports com.example.thementaltheraphyhelthfinal;
+}
