e43467aa080ade294844771b191ab8405e01c545
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-03-23 19:29:10 +0530

    created the login page

--- src/main/java/com/example/thementaltheraphyhelthfinal/controller/LoginContro.java
+++ src/main/java/com/example/thementaltheraphyhelthfinal/controller/LoginContro.java
@@ -1,5 +1,20 @@
 package com.example.thementaltheraphyhelthfinal.controller;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginContro {
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtPw;
+
+    @FXML
+    void LogIn(ActionEvent event) {
+        //HERE CHECK THE PASSWORD AND EMAIL LOG INTO LOG IN TO THE DASHBOARD
+
+    }
 }
