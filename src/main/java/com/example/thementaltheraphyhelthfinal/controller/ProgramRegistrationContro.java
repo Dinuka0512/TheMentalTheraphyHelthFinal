335e5255ad56297d66bd0fe50cb3335a9c34fa6d
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-04-18 23:37:42 +0530

    Created the Registration controller class

--- src/main/java/com/example/thementaltheraphyhelthfinal/controller/ProgramRegistrationContro.java
+++ src/main/java/com/example/thementaltheraphyhelthfinal/controller/ProgramRegistrationContro.java
@@ -1,4 +1,93 @@
 package com.example.thementaltheraphyhelthfinal.controller;
 
-public class ProgramRegistrationContro {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProgramRegistrationContro implements Initializable {
+    @FXML
+    private TableColumn<?, ?> colProFee;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private ComboBox<?> comboProgram;
+
+    @FXML
+    private TableColumn<?, ?> colPatientId;
+
+    @FXML
+    private Label lblId;
+
+    @FXML
+    private TableColumn<?, ?> colPatientName;
+
+    @FXML
+    private Label lblFee;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ComboBox<?> comboPatient;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private TableColumn<?, ?> colProName;
+
+    @FXML
+    private Label lblProgramName;
+
+    @FXML
+    private TableColumn<?, ?> colProId;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private Label lblPatientName;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private TableView<?> tblRegistration;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    void gettableDetails(ActionEvent event) {
+
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reset(ActionEvent event) {
+
+    }
+
 }
